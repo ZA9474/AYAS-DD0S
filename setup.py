02f38cc1aa224528e535d675104da7f98a93be5a
OZKON24 <za9474@yahoo.com>
2024-11-15 08:23:36 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+
+print("""[0] pip\n[1] pip3\nWhich one do you use?""")
+
+c = input(">>>: ")
+if c == "0":
+     os.system("apt-get install docker")
+     os.system("pip install torch")
+     os.system("pip install wheel")
+     os.system("pip install flake8 pytest")
+     os.system("git pull")
+elif c == "1":
+     os.system("apt-get install docker")
+     os.system("pip3 install torch")
+     os.system("pip3 install wheel")
+     os.system("pip3 install flake8 pytest")
+     os.system("git pull")
+     print("Done.")
